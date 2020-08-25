d5b6d554380d0434da76f1bd5a3154158f5f4095
Andrew <15331990+ahuang11@users.noreply.github.com>
2020-08-25 15:58:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='tropycal_model_hack',
+      version='0.0.1',
+      description='Inserting model data into tropycal storm dict',
+      url='https://github.com/ahuang11/tropycal_model_hack',
+      packages=['tropycal_model_hack'],
+      include_package_data=True,
+      install_requires=[
+                        'pandas',
+                        'tropycal',
+                        ],
+      keywords=['tropycal', 'model', 'hack'],
+      entry_points={
+          'console_scripts': [
+          ]
+      },
+      zip_safe=True)
